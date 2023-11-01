18f36c15798b56339f84932675d5753f764c8fa8
Greg <gregory.vs123@gmail.com>
2023-11-01 07:57:44 -0300

    add packages

--- app/.next/static/chunks/webpack.js
+++ app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fed057ebb82cd82c"; }
+/******/ 		__webpack_require__.h = function() { return "66ff5088e292f7fa"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
